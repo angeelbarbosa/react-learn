22fb4b5e138ad0b1842c10e862b09d3ea92a752d
Angel Barbosa <angelbarbosa211@gmail.com>
2023-04-11 17:17:45 -0500

    adding twitter user handle mockup

---
+++ projects/00-hello-world/src/App.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import './App.css'
+import { TwitterFollowCard } from './TwitterFollowCard.jsx'
+
+
+
+export function App(){
+
+
+  return (
+    <>
+      <section className="App">
+        <TwitterFollowCard
+
+          userName="realmadrid" initialIsFollowing={true}
+          name="Real Madrid C.F"
+        />
+        <TwitterFollowCard
+
+          userName="Benzema"
+          name="Karim Benzema"
+        />
+        <TwitterFollowCard
+
+          userName="lukamodric10"
+          name="Luka Modrić"
+        />
+        <TwitterFollowCard
+
+          userName="vinijr"
+          name="Vini Jr."
+        />
+      </section>
+    </>
+  );
+}
